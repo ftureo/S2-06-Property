d847be1109e64a69fee84ba3a52486e19f64cfc8
mlcas <martincastro88@hotmail.com>
2022-05-27 13:26:41 -0700

    forgot password

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,7 +3,10 @@ import express from "express";
 import {
   createNewUser,
   authenticate,
-  confirmed
+  confirmed,
+  forgotPassword,
+  checkToken,
+  newToken
 } from "../controllers/users.controllers.js";
 
 const router = express.Router();
@@ -11,5 +14,8 @@ const router = express.Router();
 router.post("/", createNewUser);
 router.post("/login", authenticate);
 router.get("/confirmed/:token", confirmed);
+router.post("/forgot-password", forgotPassword);
+router.get("/forgot-password/:token", checkToken);
+router.post("/forgot-password/:token", newToken);
 
 export default router;
